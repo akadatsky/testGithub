f9b43c89bb0525bb0c7ada44ee4d303e9a58a33a
HILLELTEACHERPC\User <polomkinanton@gmail.com>
2018-08-28 17:57:47 +0300

    Update doSomething

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        testMethod();
+    }
+
+    private static void testMethod() {
+        // something
+        System.out.println("TEST");
+        // test
+        System.out.println("TEST2");
     }
 
 }
