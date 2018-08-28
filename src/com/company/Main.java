e962a8edfebf6318a207c5b067c5cfe6113611b3
HILLELTEACHERPC\User <polomkinanton@gmail.com>
2018-08-28 17:37:59 +0300

    initial committ

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        doSomething();
+    }
+
+    private static void doSomething() {
+        System.out.println("TEST");
+    }
+
+}
