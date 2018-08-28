efcd85dae09b3221f69a92aab100cd1120a7c68c
Alexandr Nikonets <nick8704@gmail.com>
2018-08-28 17:57:33 +0300

    make konflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        doSomething();
-    }
-
-    private static void doSomething() {
-        System.out.println("TEST");
     }
 
 }
